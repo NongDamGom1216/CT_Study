48f2140117c1b0ef301d090b4ccba7bfe595c706
NongDamGom1216 <hyun5735@naver.com>
2022-05-16 01:05:33 +0900

    220516

---
+++ SW_Expert/일요일.py
@@ -0,0 +1,15 @@
+from cgi import test
+
+
+T = int(input())
+
+DAY = ["MON","TUE","WED","THU","FRI","SAT","SUN"]
+
+for test_case in range(1, T + 1):
+    day = input()
+    if day=="SUN":
+        print(f'#{test_case} 7')
+        continue
+    next_day=6-DAY.index(day)
+    print(f'#{test_case} {next_day}')
+    
